89a91cc966b3883dce12c8dddc9aabcab5501608
Ezequiel Gonzalez Garcia <ezeezegg@gmail.com>
2015-09-18 17:48:14 -0500

    test for add image

--- app/src/main/java/me/ezeezegg/aiesecFlorianapolis/adapters/infoAdapter.java
+++ app/src/main/java/me/ezeezegg/aiesecFlorianapolis/adapters/infoAdapter.java
@@ -1,13 +1,17 @@
 package me.ezeezegg.aiesecFlorianapolis.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import me.ezeezegg.aiesecFlorianapolis.R;
@@ -63,11 +67,36 @@ public class infoAdapter extends RecyclerView.Adapter<infoAdapter.ViewHolder> {
         viewHolder.title.setText(info.getTitle());
         viewHolder.authors.setText(info.getAuthors());
         viewHolder.date.setText(info.getDate());
+        new DownloadImageTask(viewHolder.image).execute(info.getImage());
         viewHolder.itemView.setTag(info);
     }
 
     @Override
     public int getItemCount() {
         return infoArrayList.size();
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
