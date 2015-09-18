8d18e7fcf6aca2f8a867c353acc9600f349217d8
Ezequiel Gonzalez Garcia <ezeezegg@gmail.com>
2015-09-18 17:06:00 -0500

    create properties en model

--- app/src/main/java/me/ezeezegg/aiesecFlorianapolis/models/Info.java
+++ app/src/main/java/me/ezeezegg/aiesecFlorianapolis/models/Info.java
@@ -5,10 +5,55 @@ package me.ezeezegg.aiesecFlorianapolis.models;
  */
 public class Info {
     private Integer id;
+    private String website;
     private String title;
-    private String address;
-    private String phone;
+    private String image;
+    private String content;
+    private String authors;
+    private String date;
     private float averageRating;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+
 
     public Integer getId() {
         return id;
@@ -24,22 +69,6 @@ public class Info {
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
     }
 
     public float getAverageRating() {
